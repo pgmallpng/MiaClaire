31ce9252629ff05602aeb0645d75cdb170c6c3ce
pgmallpng <pgmallpng@gmail.com>
2026-05-05 15:41:10 +1000

    Update contact-config.js

--- contact-config.js
+++ contact-config.js
@@ -4,6 +4,6 @@ var CONTACT_CONFIG = {
         { name: "Telegram Mia",   url: "https://t.me/m/B4lWJCOZOWVl",      icon: "📱" },
         { name: "Telegram",  url: "https://t.me/Mia_Claire_33",       icon: "📱" },
         { name: "Signal",            url: "https://signal.me/#eu/iawB5NudJJf08YQCU7heIQjavTxIyfiTVK1DT-8wZw-neEgqB89G7Iq7KwerJkSZ",     icon: "💬" },
-        { name: "WhatsApp",         url: "https://wa.me/85257772131",   icon: "💬" }
+        { name: "WhatsApp",         url: "https://wa.me/85253727559",   icon: "💬" }
     ]
 };
